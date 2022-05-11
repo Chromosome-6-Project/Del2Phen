f2357a3b1208bba4565e265c10e73a58109e6bb2
TDMedina <tylerdanmedina@gmail.com>
2022-05-10 23:49:51 -0500

    Moved phenotype homogeneity things to new module.
    Also code cleanup and minor changes.

--- chr6.py
+++ chr6.py
@@ -16,9 +16,12 @@ import pandas as pd
 from pronto import Ontology, Term
 
 from data_objects import Patient, PatientDatabase, GenomeDict
-
 import gene_set
-
+from phenotype_homogeneity import (
+    PhenotypeHomogeneity,
+    GroupPhenotypeHomogeneity,
+    phenotype_homo_test
+    )
 from utilities import jaccard, length_of_range_intersects, merge_range_list
 
 
@@ -195,7 +198,7 @@ class DataManager:
             print(f"{geno:<13}{pheno:<14}{sums.count((geno,pheno)):3}")
 
     @staticmethod
-    def make_UCSC_browser_tracks(patients, out, filter_chrom=None):
+    def make_ucsc_browser_tracks(patients, out, filter_chrom=None):
         """Write UCSC BED file of patient CNVs."""
         writer = ["browser hide all position chr6\n"]
         patient_list = sorted(
@@ -218,6 +221,7 @@ class DataManager:
         with open(drop_list_file) as infile:
             drop_list = infile.readlines()
         drop_list = [name.strip() for name in drop_list]
+        drop_list = {name for name in drop_list if not name.startswith("#")}
         return drop_list
 
     @staticmethod
@@ -462,13 +466,15 @@ class ComparisonTable:
     def make_patient_intersection_group(self, patient_id, length_similarity=0,
                                         loci_similarity=0, gene_similarity=0,
                                         hi_similarity=0, hpo_similarity=0,
-                                        include_self=True):
+                                        include_self=True, as_patient_database=True):
         intersections = self.filter_patient_intersections(
             patient_id, length_similarity, loci_similarity, gene_similarity,
             hi_similarity, hpo_similarity, include_self
             )
         patients = [intersection.get_other_patient(patient_id)
                     for intersection in intersections.values()]
+        if as_patient_database:
+            patients = PatientDatabase({patient.id: patient for patient in patients})
         return patients
 
     def test_phenotype_homogeneities(self, patient_id, phenotypes, length_similarity=0,
@@ -476,17 +482,17 @@ class ComparisonTable:
                                      hi_similarity=0, hpo_similarity=0):
         group = self.make_patient_intersection_group(
             patient_id, length_similarity, loci_similarity,
-            gene_similarity, hi_similarity, hpo_similarity
-            )
-        size = len(group)
+            gene_similarity, hi_similarity, hpo_similarity,
+            as_patient_database=True)
+        size = group.size
         homogeneities = {
             phenotype: sum([patient.hpo[phenotype] == "T" for patient in group
                             if phenotype in patient.hpo])
             for phenotype in phenotypes
             }
-        homogeneities = {phenotype: PhenotypeHomogeneity(patient_id, phenotype, size, prevalence)
+        homogeneities = {phenotype: PhenotypeHomogeneity(patient_id, group, phenotype, size, prevalence)
                          for phenotype, prevalence in homogeneities.items()}
-        homogeneities = GroupPhenotypeHomogeneity(homogeneities)
+        homogeneities = GroupPhenotypeHomogeneity(patient_id, group, homogeneities)
         return homogeneities
 
     def test_all_homogeneities(self, phenotypes, length_similarity=0,
@@ -1018,77 +1024,6 @@ PredictInfo = namedtuple("PredictionInfo",
                          ["count", "frequency", "TP"])
 
 
-class PhenotypeHomogeneity:
-    def __init__(self, group_id, phenotype, group_size, prevalence,):
-        self.group_id = group_id
-        self.phenotype = phenotype
-        self.group_size = group_size
-        self.prevalence = prevalence
-        self.relative_prevalence = 0
-        if self.group_size > 0:
-            self.relative_prevalence = self.prevalence/self.group_size
-
-    def __repr__(self):
-        string = (f"PhenotypeHomogeneity("
-                  f"group_id={self.group_id}, "
-                  f"phenotype='{self.phenotype.name}', "
-                  f"group_size={self.group_size}, "
-                  f"prevalence={self.prevalence} ({self.relative_prevalence:.2%}))")
-        return string
-
-
-class GroupPhenotypeHomogeneity:
-    def __init__(self, phenotype_homogeneities):
-        self.homogeneities = phenotype_homogeneities
-        self.group_id = self.set_group_id()
-        self.group_size = self.set_group_size()
-        self.phenotypes = list(self.homogeneities.keys())
-        self.num_phenotypes = len(self.phenotypes)
-        self.num_present = self.count_present_phenotypes()
-        self.proportion_present = self.calculate_overall_proportion_present()
-
-    def __str__(self):
-        string = (f"GroupPhenotypeHomogeneity(num_phenotypes={self.num_phenotypes}, "
-                  f"phenotypes_present={self.num_present} "
-                  f"({self.proportion_present:.2%}))")
-        return string
-
-    def count_present_phenotypes(self):
-        counts = sum([phenotype.prevalence >= 1
-                      for phenotype in self.homogeneities.values()])
-        return counts
-
-    def count_phenotypes_above_relative_prevalence(self, threshold):
-        counts = sum([phenotype.relative_prevalence >= threshold
-                      for phenotype in self.homogeneities.values()])
-        return counts
-
-    def calculate_homogeneity(self, threshold):
-        if self.num_present == 0:
-            return 0
-        counts = self.count_phenotypes_above_relative_prevalence(threshold)
-        homogeneity = counts / self.num_present
-        return homogeneity
-
-    def calculate_overall_proportion_present(self):
-        prevalence = self.num_present / self.num_phenotypes
-        return prevalence
-
-    def set_group_size(self):
-        group_size = {x.group_size for x in self.homogeneities.values()}
-        if len(group_size) != 1:
-            raise ValueError("PhenotypeHomogeneities have different group sizes.")
-        group_size = list(group_size)[0]
-        return group_size
-
-    def set_group_id(self):
-        group_id = {x.group_id for x in self.homogeneities.values()}
-        if len(group_id) != 1:
-            raise ValueError("PhenotypeHomogeneities have different group IDs.")
-        group_id = list(group_id)[0]
-        return group_id
-
-
 # %% Main
 def main(geno_file, pheno_file):
     """Run main."""
@@ -1177,27 +1112,6 @@ def predict_test(comparison, patient_list="C:/Users/Ty/Documents/Chr6/Predict_te
     return all_tests, aafkes_tests
 
 
-def phenotype_homo_test(comparison, ontology, hi_similarity, phenotypes=None):
-    if phenotypes is None:
-        with open("/home/tyler/Documents/Chr6_docs/Phenotype_Homogeneity/selected_phenotypes_hpos.txt") as infile:
-            selected_hpos = infile.readlines()
-        selected_hpos = [ontology[x.strip().split("\t")[1]] for x in selected_hpos]
-    else:
-        selected_hpos = phenotypes
-    homos = comparison.test_all_homogeneities(selected_hpos, hi_similarity=hi_similarity)[0]
-    pheno_data = {hpo: [] for hpo in selected_hpos}
-    for ID, homogroup in homos.items():
-        for pheno, homo in homogroup.homogeneities.items():
-            pheno_data[pheno].append(homo)
-    table_dict = {phenotype.name:
-                      {f"{homo.group_id} ({homo.group_size})": homo.relative_prevalence
-                       for homo in sorted(homos, key=lambda x: x.group_size, reverse=True)}
-                  for phenotype, homos in pheno_data.items()}
-    table_dict["Phenotype Homogeneity"] = {f"{homo.group_id} ({homo.group_size})": homo.calculate_homogeneity(.2)
-                                           for homo in sorted(homos.values(), key=lambda x: x.group_size)}
-    return table_dict, selected_hpos, pheno_data, homos
-
-
 if __name__ == "__main__":
     # import sys
     # main(sys.argv[1], sys.argv[2])
@@ -1209,4 +1123,4 @@ if __name__ == "__main__":
         drop_list_file="/home/tyler/Documents/Chr6_docs/PatientData/drop_list.txt",
         expand_hpos=True
         )
-    table_dict, selected_hpos, pheno_data, homos = phenotype_homo_test(my_comparison, my_ontology, .85)
+    table, selected_hpos, homos = phenotype_homo_test(my_comparison, my_ontology, .85, .2, 2)
