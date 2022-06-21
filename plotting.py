7ad4453ddbeaea079b6e6cc9974678cef43ae91e
TDMedina <tylerdanmedina@gmail.com>
2022-06-20 21:07:40 -0700

    Rough draft of new plots.

--- plotting.py
+++ plotting.py
@@ -10,12 +10,31 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
 import plotly.io as pio
 
 import network
 from utilities import overlap
 
 pio.renderers.default = "browser"
+
+GA_COLORS = ["#F0A3FF", "#FF0010", "#2BCE48", "#FFCC99", "#FFFF80",
+             "#100AFF", "#5EF1F2", "#990000", "#C20088", "#003380",
+             "#426600", "#19A405", "#FFA8BB", "#0075DC", "#808080",
+             "#FFE100", "#8F7C00", "#94FFB5", "#4C005C", "#00998F",
+             "#FF5000", "#993F00", "#005C31", "#9DCC00", "#191919",
+             "#E0FF66"]
+
+
+def hex_to_rgb(hex):
+    rgb = tuple(int(hex[i:i+2], 16) for i in range(1, 6, 2))
+    return rgb
+
+
+def hex_to_rgba(hex, alpha):
+    rgba = tuple([int(hex[i:i+2], 16) for i in range(1, 6, 2)] + [alpha])
+    return rgba
+
 
 # XXX: Deprecated.
 def make_array(table):
@@ -185,6 +204,178 @@ def plot_median_degree_vs_hi_similarity(comparison_table):
     # return
     return fig
 
-def plot_phenotype_homogeneity_vs_hi_similarity(comparison_table, hi_sim_values):
+# def plot_phenotype_homogeneity_vs_hi_similarity(comparison_table, hi_sim_values):
 
 
+def plot_min_degree_count_vs_hi_score(comparison, hi_scores, min_degrees):
+    fig = go.Figure()
+    for min_degree in min_degrees:
+        graph = network.make_nx_graph_from_comparisons(comparison)
+        points = []
+        for hi_score in hi_scores:
+            filtered = network.filter_graph_edges(graph, hi_gene_sim_threshold=hi_score)
+            degree_count = network.count_nodes_over_n_degree(filtered, min_degree)
+            points.append(degree_count)
+        fig.add_trace(go.Scatter(x=hi_scores, y=points, name=f"n ≥ {min_degree}"))
+    fig.update_layout(title="Number of patients with minimum connections vs. minHIGSS",
+                      xaxis_title="Minimum HI gene similarity score",
+                      yaxis_title="Number of patients with ≥ n connections",
+                      legend_title="Connection Threshold (n)",
+                      hovermode="x unified")
+    return fig
+
+
+def ph_score_vs_group_size(comparison, phenotypes, hi_scores,
+                           rel_threshold=0.2, abs_threshold=2, min_size=5):
+    thresh = (rel_threshold, abs_threshold)
+    fig = go.Figure()
+    for hi_score in sorted(hi_scores, reverse=True):
+        _, group_phs, _ = comparison.test_all_homogeneities(
+            phenotypes=phenotypes,
+            hi_similarity=hi_score,
+            group_size_threshold=min_size
+            )
+        data = [(group_ph.group_size, group_ph.calculate_homogeneity(*thresh))
+                for group_ph in group_phs.values()]
+        data.sort(key=lambda x: x[0])
+        xs, ys = zip(*data)
+        fig.add_trace(go.Scatter(x=xs, y=ys, mode="markers", name=f"HI ≥ {hi_score}"))
+    fig.update_layout(title=f"PH Score vs Group Size, for groups ≥ {min_size}",
+                      xaxis_title="Group size",
+                      yaxis_title="PH score",
+                      legend_title="HI Threshold")
+    return fig
+
+
+def ph_score_histograms_by_hi_score(comparison, phenotypes, hi_scores,
+                                    rel_threshold=0.2, abs_threshold=2, min_size=5):
+    thresh = (rel_threshold, abs_threshold)
+    fig = go.Figure()
+    for hi_score in sorted(hi_scores, reverse=True):
+        _, group_phs, _ = comparison.test_all_homogeneities(
+            phenotypes=phenotypes,
+            hi_similarity=hi_score,
+            group_size_threshold=min_size
+            )
+        data = [group_ph.calculate_homogeneity(*thresh)
+                for group_ph in group_phs.values()]
+        data = [sum([x >= i for x in data]) for i in np.linspace(0, 1, 51)]
+        data_len = len(group_phs)
+        perc = [point/data_len for point in data]
+        fig.add_trace(go.Scatter(x=np.linspace(0, 1, 51), y=data,
+                                 name=f"HI ≥ {hi_score}", customdata=perc,
+                                 hovertemplate="PH ≥ %{x}: %{y} (%{customdata:.2%})"))
+    fig.update_layout(title=f"Number of Patients with Minimum PH Score,"
+                            f" for groups ≥ {min_size}",
+                      xaxis_title="Minimum PH score",
+                      yaxis_title="Number of Patients",
+                      legend_title="HI Threshold",
+                      hovermode="x unified")
+    return fig
+
+
+def ph_score_histograms_by_hi_score_w_error(comparison, phenotypes, hi_scores,
+                                            rel_threshold=0.2, abs_threshold=2,
+                                            min_size=5, random_combinations=50):
+    thresh = (rel_threshold, abs_threshold)
+    fig = go.Figure()
+    x_axis = np.linspace(0, 1, 51)
+
+    for i, hi_score in enumerate(sorted(hi_scores, reverse=True)):
+        _, group_phs, _ = comparison.test_all_homogeneities(
+            phenotypes=phenotypes,
+            hi_similarity=hi_score,
+            group_size_threshold=min_size
+            )
+        data = [group_ph.calculate_homogeneity(*thresh)
+                for group_ph in group_phs.values()]
+        data = np.array([sum([x >= j for x in data]) for j in x_axis])
+        data_len = len(group_phs)
+        perc = data / data_len
+
+        error_data = np.ndarray([random_combinations+1, 51])
+        error_data[0] = data
+        for perm in range(1, random_combinations+1):
+            phenos= np.random.choice(phenotypes, 20, replace=False)
+            _, group_phs, _ = comparison.test_all_homogeneities(
+                phenotypes=phenos,
+                hi_similarity=hi_score,
+                group_size_threshold=min_size
+                )
+            error_perm_data = [group_ph.calculate_homogeneity(*thresh)
+                               for group_ph in group_phs.values()]
+            error_data[perm] = [sum([x >= j for x in error_perm_data])
+                                for j in np.linspace(0, 1, 51)]
+
+        error_min = data - error_data.min(axis=0)
+        error_max = error_data.max(axis=0) - data
+        error_vals = np.concatenate([error_data.max(axis=0),
+                                     error_data.min(axis=0)[::-1]])
+        custom_data = pd.DataFrame(dict(perc=perc,
+                                        error_min=error_min,
+                                        error_max=error_max))
+
+        fig.add_trace(go.Scatter(x=x_axis, y=data,
+                                 name=f"HI ≥ {hi_score}",
+                                 customdata=custom_data,
+                                 hovertemplate="PH ≥ %{x}: %{y} " 
+                                               "(%{customdata[0]:.2%})"
+                                               " +%{customdata[2]}/-%{customdata[1]}",
+                                 legendgroup=f"{hi_score}",
+                                 line_color=GA_COLORS[i],
+                                 ))
+        fig.add_trace(go.Scatter(x=np.concatenate([x_axis, x_axis[::-1]]),
+                                 y=error_vals,
+                                 fill='toself',
+                                 line_color="rgba(255,255,255,0)",
+                                 fillcolor=f"rgba{hex_to_rgba(GA_COLORS[i], 0.3)}",
+                                 hoverinfo="skip",
+                                 legendgroup=f"{hi_score}",
+                                 showlegend=False
+                                 ))
+    fig.update_layout(title=f"Number of Patients with Minimum PH Score,"
+                            f" for groups ≥ {min_size}",
+                      xaxis_title="Minimum PH score",
+                      yaxis_title="Number of Patients",
+                      legend_title="HI Threshold",
+                      hovermode="x unified")
+    return fig
+
+
+def ph_score_histograms_by_hi_score_by_area(comparison, phenotypes, hi_scores,
+                                            rel_threshold=0.2, abs_threshold=2, min_size=5):
+    thresh = (rel_threshold, abs_threshold)
+    divider = 57038355
+    fig = make_subplots(1, 3)
+
+    for hi_score in sorted(hi_scores, reverse=True):
+        _, group_phs, _ = comparison.test_all_homogeneities(
+            phenotypes=phenotypes,
+            hi_similarity=hi_score,
+            group_size_threshold=min_size
+            )
+
+        region_data = [[], [], []]
+        for group_ph in group_phs.values():
+            median_locus = group_ph.patients.get_median_cnv_position("6")
+            region = (divider < median_locus) + (divider*2 < median_locus)
+            region_data[region].append(group_ph)
+
+        for sub, data in enumerate(region_data, start=1):
+            data = [group_ph.calculate_homogeneity(*thresh) for group_ph in data]
+            data = [sum([x >= i for x in data]) for i in np.linspace(0, 1, 51)]
+            data_len = len(data)
+            perc = [point/data_len for point in data]
+            fig.add_trace(go.Scatter(x=np.linspace(0, 1, 51), y=data,
+                                     name=f"HI ≥ {hi_score}", customdata=perc,
+                                     hovertemplate="PH ≥ %{x}: %{y} (%{customdata:.2%})",
+                                     legendgroup=f"{hi_score}"),
+                          row=1, col=sub)
+
+    fig.update_layout(title=f"Number of Patients with Minimum PH Score,"
+                            f" for groups ≥ {min_size}",
+                      xaxis_title="Minimum PH score",
+                      yaxis_title="Number of Patients",
+                      legend_title="HI Threshold",
+                      hovermode="x unified")
+    return fig
